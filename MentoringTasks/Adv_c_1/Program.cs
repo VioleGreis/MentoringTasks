4dee3d1100e760b0d41e96c0f0d8fa16c77c5399
Anton Paulianok <Anton_Paulianok@epam.com>
2017-07-06 14:20:47 +0300

    [Tasks] 1

---
+++ MentoringTasks/Adv_c_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv_c_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        struct InfoData : IInfoData
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        class Source
+        {
+            internal void CheckAndProceed(IEnumerable<IInfoData> data)
+            {
+                var dest = new Destination();
+                dest.ProceedData(data);
+            }
+        }
+
+        class Destination
+        {
+            internal void ProceedData(IEnumerable<IInfoData> data)
+            {
+                foreach (var item in data)
+                {
+                }
+            }
+        }
+
+        public interface IInfoData
+        {
+            string FirstName { get; set; }
+            string LastName { get; set; }
+        }
+    }
+}
