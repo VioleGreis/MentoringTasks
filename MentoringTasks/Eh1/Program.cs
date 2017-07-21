5dfdc58d7bf12407d46630330e5e98c57ae2342e
Anton Paulianok <Anton_Paulianok@epam.com>
2017-07-21 13:11:37 +0300

    EH1

---
+++ MentoringTasks/Eh1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eh1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = string.Empty;
+            while (str != "E" || str != "Exit")
+            {
+                str = Console.ReadLine();
+                Console.WriteLine(str == string.Empty ? "Empty" : str?[0].ToString());
+            }
+            Console.Read();
+        }
+    }
+}
