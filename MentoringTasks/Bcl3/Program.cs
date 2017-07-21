03df282f5c2fabb5328c8dd7d152f0455406b6f8
Anton Paulianok <Anton_Paulianok@epam.com>
2017-07-21 12:35:52 +0300

    Bcl3

---
+++ MentoringTasks/Bcl3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bcl3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new List<string> { "a", "b", "a", "b", "a", "b", "a", "b", "a", "b", "a", "b", "a", "b", "Odd", "Even" };
+            Console.WriteLine(StringEvenUnion(strings));
+            Console.Read();
+        }
+
+        static string StringEvenUnion(IEnumerable<string> strings)
+        {
+            var list = strings.ToList();
+            var sb = new StringBuilder();
+            for (var i = 1; i < list.Count; i += 2)
+            {
+                sb.Append(list[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
