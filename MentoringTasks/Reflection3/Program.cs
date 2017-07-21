d23921774458ba2e979276bcd371700d54abfd7a
Anton Paulianok <Anton_Paulianok@epam.com>
2017-07-21 13:52:15 +0300

    Reflection 1

---
+++ MentoringTasks/Reflection3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = CreateList(typeof(string));
+            for (string i = "0"; i != "00000000000"; i += "0")
+            {
+                list.Add(i);
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+
+        private static IList CreateList(Type type) => 
+            (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(type));
+    }
+}
