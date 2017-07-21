3295bce3218b1a6fa8887b45ce27baf6d694c1c1
Anton Paulianok <Anton_Paulianok@epam.com>
2017-07-21 11:28:30 +0300

    [Tasks] Bcl1

---
+++ MentoringTasks/Bcl1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bcl1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAttribute = (MyAttr)Attribute.GetCustomAttribute(typeof(MyClass), typeof(MyAttr));
+            Console.WriteLine($"{myAttribute.Name} <-> {myAttribute.Email}");
+            Console.Read();
+        }
+
+        [MyAttr(2,"email")]
+        class MyClass
+        {
+
+        }
+
+        [AttributeUsage(AttributeTargets.Class)]
+        class MyAttr : Attribute 
+        {
+            public int Name { get; set; }
+            public string Email { get; set; }
+            public MyAttr(int name, string email)
+            {
+                Name = name;
+                Email = email;
+            }
+        }
+    }
+}
