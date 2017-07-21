0d68e36fa306ed3809984a4f722ce76c57a587f9
Anton Paulianok <Anton_Paulianok@epam.com>
2017-07-21 12:57:41 +0300

    Bcl4

---
+++ MentoringTasks/Bcl4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bcl4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = new TrueBirthDate();
+            temp.BirthTime = DateTime.Now;
+            Console.WriteLine($"value {temp.BirthTime}");
+            temp.BirthTime = DateTime.MinValue;
+            Console.WriteLine($"value {temp.BirthTime}");
+            Console.Read();
+        }
+
+        class TrueBirthDate
+        {
+            private DateTime? _date;
+            public DateTime? BirthTime
+            {
+                get { return _date; }
+                set 
+                {
+                    if (value != null && (value.Value.Year < 1900 || value.Value.Year > 2017)) _date = null;
+                    else _date = value;
+                }
+            }
+        }
+    }
+}
