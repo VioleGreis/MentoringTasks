6af50f0de1b5d3d8f5ff3b115c01f8cc447f4092
Anton Paulianok <Anton_Paulianok@epam.com>
2017-07-21 12:11:55 +0300

    Bcl2

---
+++ MentoringTasks/Bcl2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bcl2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new List<string>{"asdsad", "gagsga", "zxczxcA", "a", "asfbb", "babsb", "yYayasy"};
+            strings = OrdinalSort(strings).ToList();
+            foreach (var i in strings)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+
+        public static IEnumerable<string> OrdinalSort(IEnumerable<string> list)
+        {
+            var sortedlist = list.ToList();
+            sortedlist.Sort(Comparison);
+            return sortedlist;
+        }
+
+        private static int Comparison(string s, string s1) => string.CompareOrdinal(s, s1);
+    }
+}
